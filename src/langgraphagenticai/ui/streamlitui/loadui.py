ec7c2ed6a2666625a09d2a96b41875ecffcc589e
shivamjaiswal424 <shivam424.sj@gmail.com>
2026-06-26 20:17:39 +0530

    Basic Chatbot Completed

---
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import os
+from src.langgraphagenticai.ui.uiconfigfile import Config
+
+class LoadStreamlitUI:
+    def __init__(self):
+        self.config = Config()
+        self.user_controls={}
+
+        
+
+    def load_streamlit_ui(self):
+        st.set_page_config(page_title=self.config.get_page_title(),layout="wide")
+        st.header(self.config.get_page_title())
+
+        with st.sidebar:
+            llm_options = self.config.get_llm_options()
+            usecase_options = self.config.get_usecase_options()
+
+            self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options)
+            
+            if self.user_controls["selected_llm"] == "Groq":
+                model_options = self.config.get_groq_model_options()
+                self.user_controls["selected_groq_model"] = st.selectbox("Select Groq Model", model_options)
+                self.user_controls["GROQ_API_KEY"]=st.session_state["GROQ_API_KEY"] = st.text_input("API Key", type="password")
+
+                if not self.user_controls["GROQ_API_KEY"]:
+                    st.warning("Please enter your Groq API Key")
+
+            self.user_controls["selected_usecase"] = st.selectbox("Select Use Case", usecase_options)
+
+        return self.user_controls
+        
+ 
